85933dfc9c5746804a220edbcf94109ae580bcb8
YoungseokOh <seok7302@gmail.com>
2021-11-22 01:18:30 +0900

    update crawling category

--- crawling.py
+++ crawling.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import utils
 import requests
-from datetime import date
+from datetime import date, datetime
 import json
 
-def day_crawling(url, date_text):
+
+def day_crawling(url, category_name, today_date, date_text):
     test_url = url
-    today_date = date.today()
     req = requests.get(test_url)
     if req.status_code == 404:
         pass
@@ -39,26 +39,30 @@ def day_crawling(url, date_text):
     data['count'] = [len(questions)]
     for n in range(len(questions)):
         data['post'].append({"question": questions[n], "answer":answer[n]})
-    with open("./캐시워크/{}/{}.json".format(date.today(), title), 'w', encoding='UTF-8-sig') as outfile:
+    with open("./{}/{}/{}.json".format(category_name, today_date, title), 'w', encoding='UTF-8-sig') as outfile:
         json.dump(data, outfile, ensure_ascii=False, indent=4)
     print(title)
     print(questions)
     print(answer)
 
+
 def main():
     # For test
+    date_str = '2021-11-19'
+    today_date = datetime.strptime(date_str, "%Y-%m-%d")
     blog_list_url = 'https://luckyquiz.tistory.com/category/'
     bs_Test = BeautifulSoup(
         urllib.request.urlopen(blog_list_url).read(), 'html.parser')
     category_len = len(bs_Test.find_all('a', attrs={'class': 'link_item'}))
     for cate_num in range(category_len):
         category_name = utils.remove_slash_nt(bs_Test.find_all('a', attrs={'class': 'link_item'})[cate_num].getText())
+        print(category_name)
         category_href = bs_Test.find_all('a', attrs={'class': 'link_item'})[cate_num]["href"]
-        save_path = './{}/{}'.format(category_name, date.today())
+        save_path = './{}/{}'.format(category_name, date_str)
         if not utils.check_exist(save_path):
             utils.make_folder(save_path)
         for page_num in range(1, 6):
-            today_date = date.today()
+            #today_date = date.today()
             date_text = '{}월{}일'.format(today_date.month, today_date.day)
             blog_list_url = 'https://luckyquiz.tistory.com/{}?page={}'.format(
                 category_href, page_num)
@@ -67,12 +71,13 @@ def main():
             for post in range(len(bs.select('.post-item'))):
                 post_title = bs.select('.post-item')[post].find('span', {'class': 'title'})
                 if not date_text in post_title.getText():
-                    print('Not today post.')
-                    break
-                blog_crawling_url = 'https://luckyquiz.tistory.com/{}?category=748685'.format(
+                    # print('Not today post.')
+                    continue
+                blog_crawling_url = 'https://luckyquiz.tistory.com/{}'.format(
                     bs.select('.post-item')[post].find('a')['href'][1:5])
                 # print(bs.select('.post-item')[0].find('a')['href'][1:5])
-                day_crawling(blog_crawling_url, date_text)
+                day_crawling(blog_crawling_url, category_name, date_str, date_text)
+
 
 if __name__ == '__main__':
     main()
