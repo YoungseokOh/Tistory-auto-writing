108cdd38564df3842fc07f48a61e3aca93a47ae6
seok436 <seok7302@gmail.com>
2022-03-03 20:47:51 +0900

    update crawling stock

---
+++ crawling_stock.py
@@ -0,0 +1,167 @@
+from pykrx import stock
+import utils
+import os
+import time
+from finta import TA
+import pandas as pd
+from datetime import datetime, timedelta, date
+from tqdm import tqdm
+
+
+def pykrx_read_csv(stock_name, Krx_Char_folder_path):
+    condition = '.csv'
+    csv_file_path = utils.search(Krx_Char_folder_path + '/' + stock_name, condition)
+    if os.path.exists(csv_file_path[0]):
+        stock_csv = pd.read_csv(csv_file_path[0], parse_dates=True)
+    else:
+        print('Can''t find csv file!')
+    return stock_csv
+    print('read done!')
+
+
+def pykrx_scratch_save_csv(ticker, date_Start, date_End, Krx_Char_folder_path):
+    stock_name = stock.get_market_ticker_name(ticker)
+    # Get OHLCV
+    df = stock.get_market_ohlcv_by_date(date_Start, date_End, ticker)
+    if df.empty:
+        return 0
+    if df['시가'].iloc[0] == 0:
+        return 0
+    df = df.reset_index()
+    df = df.rename(columns={'날짜': 'date', '시가': 'open', '고가': 'high', '저가': 'low', '종가': 'close', '거래량': 'volume'})
+    if not os.path.exists(f"{Krx_Char_folder_path}/{stock_name}"):
+        os.makedirs(f"{Krx_Char_folder_path}/{stock_name}")
+    df.to_csv(f"{Krx_Char_folder_path}/{stock_name}/{ticker}.csv", sep=',', na_rep='0', index=False, header=True)
+
+
+def pykrx_scratch(date_Start, date_End, Krx_Char_folder_path):
+    print(f"* KRX Chart Folder Path >>> {Krx_Char_folder_path}")
+    print(f"* Reading Daily Chart >>> {date_Start} - {date_End}")
+    # create main folder
+    if not os.path.exists(Krx_Char_folder_path):
+        os.mkdir(Krx_Char_folder_path)
+    # ticker scratch
+    KOSPI_ticker_list = stock.get_market_ticker_list(market="KOSPI")
+    KOSDAQ_ticker_list = stock.get_market_ticker_list(market="KOSDAQ")
+
+    # KOSPI save as csv
+    print('Reading KOSPI...')
+    time.sleep(0.5)
+    for ticker_KOSPI in tqdm(KOSPI_ticker_list, desc="from KOSPI"):
+        pykrx_scratch_save_csv(ticker_KOSPI, date_Start, date_End, Krx_Char_folder_path)
+
+    # KOSDAQ save as csv
+    print('Reading KOSDAQ...')
+    time.sleep(0.5)
+    for ticker_KOSDAQ in tqdm(KOSDAQ_ticker_list, desc="from KOSDAQ"):
+        pykrx_scratch_save_csv(ticker_KOSDAQ, date_Start, date_End, Krx_Char_folder_path)
+    print('Scratching daily chart is done!')
+
+
+def stock_52w_update(path, date):
+    krx_list = os.listdir(path)
+    today_date = utils.get_today_ymd()
+    if date == today_date:
+        one_year_ago = datetime.now() - timedelta(days=365)
+    else:
+        one_year_ago = utils.strdate_convert(date) - timedelta(days=365)
+    stock_list_52w = []
+    count = 0
+    for stock_name in tqdm(krx_list):
+        if '스팩' in stock_name:
+            continue
+        if '리츠' in stock_name:
+            continue
+        stock_csv = pykrx_read_csv(stock_name, path)
+        if not (stock_csv['date'] <= date).any():
+            continue
+        stock_csv = stock_csv[stock_csv['date'] <= date]
+        close_price_day = stock_csv.iloc[len(stock_csv) - 1]['close']
+        stock_52w_csv = stock_csv[stock_csv['date'] >= one_year_ago.strftime("%Y-%m-%d")]
+        high_price_52w = max(stock_52w_csv['high'].to_numpy())
+        last_open_day = stock_csv['date'].tail(1)
+        stock_date_52w = stock_52w_csv[stock_52w_csv['high'] == max(stock_52w_csv['high'])]['date']
+        if high_price_52w == 0:
+            # print('This stock has issue for administration.')
+            continue
+        if last_open_day.iloc[0] == stock_date_52w.iloc[0]:
+            continue
+        # print(stock_date_52w)
+        if high_price_52w > close_price_day:
+            gap_percentage = round(((high_price_52w / close_price_day) * 100) - 100, 4)
+            stock_list_52w.append([str(stock_name), str(stock_date_52w.iloc[0]), float(gap_percentage), high_price_52w])
+            # print('Stock : {}, 52w high date : {}, Gap : {}%, 52w high price : {}'.format(stock_name,
+            # stock_date_52w.iloc[0], gap_percentage, high_price_52w))
+            count += 1
+        elif high_price_52w < close_price_day:
+            gap_percentage = round(((close_price_day / high_price_52w) * 100) - 100, 4)
+            # print('{} is already hit the 52 weeks target! Going up {}%'.format(stock_name, gap_percentage))
+    df = pd.DataFrame(stock_list_52w)
+    df = df.rename(columns={0: 'stock', 1: 'date', 2: 'gap', 3: 'high'})
+    df_csv = df.sort_values(by='gap', ascending=True)
+    df_csv.to_csv('results/this_year/' + '52_weeks_analysis_{}.csv'.format(date),
+                  encoding='utf-8', index=False, header=True)
+    return df_csv
+
+
+def base_year_high_52_weeks(df, base_year, date):
+    stock_csv = df
+    stock_date_52w = stock_csv[stock_csv['high'] == max(stock_csv['high'])]['date']
+    stock_csv = stock_csv[stock_csv['date'] < base_year]
+    if not os.path.exists('results/base_year/{}'.format(base_year)):
+        os.makedirs('results/base_year/{}'.format(base_year))
+    stock_csv.to_csv(
+        'results/base_year/{}'.format(base_year) + '/' + '52_weeks_analysis_{}_before_{}.csv'.format(date,
+                                                                           base_year),
+        encoding='utf-8', index=False, header=True)
+    return stock_csv
+
+
+def cal_technical_indicator_name(stock_name, path):
+    stock_csv = pykrx_read_csv(stock_name, path)
+    stock_csv['ema7'] = TA.EMA(stock_csv, 7)
+    stock_csv['ema50'] = TA.EMA(stock_csv, 50)
+    stock_csv['ema99'] = TA.EMA(stock_csv, 99)
+    stock_csv['upper_band'] = TA.BBANDS(stock_csv)['BB_UPPER']
+    stock_csv['lower_band'] = TA.BBANDS(stock_csv)['BB_LOWER']
+    stock_csv['rsi'] = TA.RSI(stock_csv, 14)
+    return stock_csv
+
+
+def main(today_date):
+    # Today date
+    date_str = datetime.strftime(today_date, "%Y-%m-%d")
+    Krx_char_folder_path = './krx_ohlcv'
+    results_this_year_path = './results/this_year/'
+    results_fig_path = './results/TI_fig/{}'.format(date_str)
+    # From 2019-01-01 to today
+    from_date = '2019-01-01'
+    # Do not search this year
+    base_year = '2022-01-01'
+    # KOSPI & KOSDAQ all stock scratch
+    # pykrx_scratch(from_date, date_str, Krx_char_folder_path)
+    utils.check_folder(results_this_year_path)
+    utils.check_folder(results_fig_path)
+    results_52w_csv = results_this_year_path + '52_weeks_analysis_' + date_str + '.csv'
+    if not os.path.exists(results_52w_csv):
+        # Hit the high in 52 weeks until today
+        print('{} - 52 weeks high update for analysis ...'.format(date_str))
+        time.sleep(0.5)
+        df_52w_csv = stock_52w_update(Krx_char_folder_path, utils.get_today_ymd())
+        # Hit the high in 52 weeks before 2020/01/01 (base year)
+        base_52w_csv = base_year_high_52_weeks(df_52w_csv, base_year, date_str)
+    else:
+        print('File is existed in results/this_year {}'.format(date_str))
+        df_52w_csv = pd.read_csv(results_52w_csv)
+        base_52w_csv = base_year_high_52_weeks(df_52w_csv, base_year, date_str)
+    top_5_stock_list = list(base_52w_csv[:10]['stock'])
+    for stock_name in top_5_stock_list:
+        stock_csv = cal_technical_indicator_name(stock_name,
+                                                 Krx_char_folder_path)
+        if stock_csv.iloc[-1]['volume'] > 0:
+            fig = utils.plot_technical_indicators(stock_name, stock_csv, 365)
+            fig.savefig('./results/TI_fig/{}/{}.png'.format(today_date, stock_name))
+
+
+if __name__ == '__main__':
+    main(date.today())
--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import os
 import errno
 import json
 import random
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from mplfinance.original_flavor import candlestick2_ohlc
+
 
 def check_folder(folder):
     try:
@@ -210,3 +215,114 @@ def shuffle_title(title):
     temp_shuffle_title = list(filter(None, temp_shuffle_title))
     title = ' '.join(temp_shuffle_title)
     return title
+
+
+def search(data_path, extension):
+    """Returns the list of files have extension (only current directory)
+    Args:
+        data_path (str): data path
+        extension (str): extension
+    Returns:
+        file_list (list): file list with path
+   """
+    file_list = []
+    for filename in os.listdir(data_path):
+        ext = os.path.splitext(filename)[-1]
+        if ext == extension:
+            file_list.append(data_path+'/'+filename)
+    return file_list
+
+
+def strdate_convert(date):
+    return datetime.strptime(date, "%Y-%m-%d")
+
+
+def get_today_ymd():
+    return datetime.today().strftime("%Y-%m-%d")
+
+
+def plot_technical_indicators(name, dataset, last_days):
+    plt.rc('font', family='NanumGothic')
+    dataset = dataset.iloc[-last_days:, :]
+
+    # dataset = dataset.reset_index()
+    dataset = dataset.set_index(dataset['date'])
+    ax = plt.subplot2grid((6,4), (0,0), rowspan=4, colspan=4)
+    ax.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+    #ax.spines['bottom'].set_color("#5998ff")
+    #ax.spines['top'].set_color("#5998ff")
+    #ax.spines['left'].set_color("#5998ff")
+    #ax.spines['right'].set_color("#5998ff")
+    ax.yaxis.label.set_color("k")
+    ax.tick_params(axis='y', colors='k')
+    plt.gca().yaxis.set_major_locator(mticker.MaxNLocator(prune='upper'))
+    ax.tick_params(axis='x', colors='w')
+    x_ = list(dataset.index)
+    x_range = []
+    col_name = []
+    for col_list in dataset.columns:
+        if "ema" in col_list:
+            col_name.append(col_list)
+    for i in range(0, int(len(x_))):
+        x_range.append(dataset['date'].iloc[i])
+        #print(x_range)
+    # Plot first subplot
+    candlestick2_ohlc(ax, dataset['open'], dataset['high'], dataset['low'], dataset['close'], width=0.5, colorup='r', colordown='b')
+    ax.plot(dataset['{}'.format(col_name[0])], label='{}'.format(col_name[0].upper()), color='g', linestyle='-.', linewidth=0.8)
+    ax.plot(dataset['{}'.format(col_name[1])], label='{}'.format(col_name[1].upper()), color='r', linestyle='-.', linewidth=0.8)
+    ax.plot(dataset['{}'.format(col_name[2])], label='{}'.format(col_name[2].upper()), color='b', linestyle='-.', linewidth=0.8)
+    if 'upper_band' in dataset.columns:
+        # ax.plot(dataset['upper_band'], label='Upper Band', color='gray', linewidth=0.25)
+        # ax.plot(dataset['lower_band'], label='Lower Band', color='gray', linewidth=0.25)
+        ax.fill_between(x_, dataset['lower_band'], dataset['upper_band'], alpha=0.15, color='yellow', label='Bollinger Band')
+    plt.title('Technical indicators for {} - last {} days.'.format(name, last_days))
+    ax.set_ylabel('KRW')
+    ax.legend()
+    #axv.ylabel('Volume')
+    # plt.xticks(x_[::30], x_range[::30])
+    # xlabels = ax.get_xticklabels()
+    # ax.set_xticklabels(xlabels, rotation=45, fontsize=7)
+
+    axv = plt.subplot2grid((6, 4), (4, 0), sharex=ax, rowspan=1, colspan=4)
+
+    axv.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    #axv.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    #axv.plot(dataset['volume'], color='c', linewidth=0.5)
+    axv.bar(range(len(dataset['volume'])), dataset['volume'], color='m', linewidth=0.5)
+
+    plt.gca().yaxis.set_major_locator(mticker.MaxNLocator(prune='upper'))
+    #axv.spines['bottom'].set_color("#5998ff")
+    #axv.spines['top'].set_color("#5998ff")
+    #axv.spines['left'].set_color("#5998ff")
+    #axv.spines['right'].set_color("#5998ff")
+    axv.tick_params(axis='x', colors='w')
+    axv.tick_params(axis='y', colors='k')
+
+    axv.set_ylabel('volume')
+    axv.yaxis.set_major_locator(mticker.MaxNLocator(nbins=5, prune='upper'))
+
+    axv1 = plt.subplot2grid((6, 4), (5, 0), sharex=ax, rowspan=1, colspan=4)
+    axv1.xaxis.set_major_locator(mticker.MaxNLocator(5))
+    #axv1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    axv1.plot(dataset['rsi'], color='black', linewidth=0.5)
+    axv1.set_yticks([30, 50, 70])
+    axv1.set_ylabel('rsi')
+
+
+    '''
+    for label in axv.xaxis.get_ticklabels():
+        label.set_rotation(45)
+    plt.setp(ax.get_xticklabels(), visible=False)
+    '''
+    #plt.setp(axv.get_xticklabels(), visible=False)
+    #axv = ax.twinx()
+    #print(dataset['volume'])
+    #
+    plt.subplots_adjust(left=.09, bottom=.14, right=.94, top=.95, wspace=.20, hspace=0)
+    # plt.legend()
+    plt.grid(True)
+    # plt.show() # Figure test
+    fig_save = plt.gcf()
+    return fig_save
